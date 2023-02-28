328305f4955ccd01b7e42c650309af0bef751049
great <great18@gmail.com>
2023-02-27 20:08:42 -0800

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -18,12 +18,12 @@ void puts2(char *str)
 		longi++;
 	}
 	t = longi - 1;
-	for(o = 0 ; o <= t ; o++)
+	for (o = 0 ; o <= t ; o++)
 	{
-		if(o % 2 == 0)
+		if (o % 2 == 0)
 	{
 		putchar(str[o]);
 	}
 	}
-	putcha('\n);
+	putchar('\n');
 }
