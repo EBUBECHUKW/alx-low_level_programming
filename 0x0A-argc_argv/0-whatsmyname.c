6d3166cc66d550b79654908e8c88c8e00ed24259
great <great18@gmail.com>
2023-03-14 16:13:23 -0700

    1-args.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,17 +4,11 @@
 /**
  * main - a program that prints its name, followed by a new line.
  * @argc: number of arguments
-
  * @argv: array of arguments
-
- *
-
  * Return: Always 0 (Success)
-
  */
 
 int main(int argc __attribute__((unused)), char *argv[])
-
 {
 
 	printf("%s\n", *argv);
